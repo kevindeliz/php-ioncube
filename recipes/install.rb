b49d21ef27f35d1971d1924aaf3967e5064d73ba
Kevin Deliz <kdeliz@babbel.com>
2015-11-02 12:27:41 +0100

    Update install.rb

--- recipes/install.rb
+++ recipes/install.rb
@@ -3,3 +3,11 @@ include_recipe 'php-ioncube::default'
 php_ioncube_install "ioncube" do
     action :install
 end
+
+package "php5-ldap" do
+  action :install
+end
+
+package "php5-gd" do
+  action :install
+end
