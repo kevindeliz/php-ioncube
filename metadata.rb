a69c0ec363c27da063ee9da0f3936886ad2bddb4
Kevin Deliz <kdeliz@babbel.com>
2015-11-02 13:01:22 +0100

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,3 @@ license          "All rights reserved"
 description      "Installs/Configures php-ioncube"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.2.0"
-
-depends "php"
